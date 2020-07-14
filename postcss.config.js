aae279cad0524809367e61a90bcbf039afd29c50
Uriel Rodolfo Ortiz Popolo <urielrp70@gmail.com>
2020-07-14 12:38:11 -0600

    Project of Tailwind was ended

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
     plugins:[
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        purgecss({
+            content: ['./**/*.html'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        }),
+        require('cssnano')({
+            preset: 'default',
+            })
     ]
 }
